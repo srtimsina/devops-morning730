85c0317a63bfb16ea28844cf638321dd4fb3e247
Suryaraj <devopsuryraj@gmail.com>
2026-09-10 02:20:42 +0000

    correction in the command

--- install.sh
+++ install.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
 sudo apt update
-sudo apt install -y docker-ce docker-ce-cli docker-compose-plugin containerd.io
+sudo apt install -y docker-ce docker-ce-cli docker-compose-plugin
 sudo systemctl start docker
 sudo systemctl status docker
